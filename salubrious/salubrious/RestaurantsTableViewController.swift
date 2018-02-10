31077bbce17148f45e92b421a7057d2626d6f4e7
Hasani Hendrix <hasanihendrix@Hasanis-MacBook-Pro.local>
2018-02-10 17:43:52 -0500

    app is able to add restaurants to Firebase Database now through authentication.

--- salubrious/salubrious/RestaurantsTableViewController.swift
+++ salubrious/salubrious/RestaurantsTableViewController.swift
@@ -20,6 +20,24 @@ class RestaurantsTableViewController: UITableViewController {
         dbRef = Database.database().reference().child("healthy-restaurants")
 
     }
+    @IBAction func addRestaurant(_ sender: Any) {
+        
+        let restaurantAlert = UIAlertController(title: "New Restaurant", message: "Enter your restaurant: ", preferredStyle: .alert)
+        restaurantAlert.addTextField { (textField:UITextField) in
+            textField.placeholder = "Your Restaurant"
+        }
+        restaurantAlert.addAction(UIAlertAction(title: "Send", style: .default, handler: { (action:UIAlertAction) in
+            if let restaurantContent = restaurantAlert.textFields?.first?.text {
+                let restaurant = Restaurant(content: restaurantContent, addedByUser: "HasaniHendrix")
+                
+                let restaurantRef = self.dbRef.child(restaurantContent.lowercased())
+                
+                restaurantRef.setValue(restaurant.toAnyObject())
+            }
+        }))
+        
+        self.present(restaurantAlert, animated:true, completion: nil)
+    }
 
 
     // MARK: - Table view data source
